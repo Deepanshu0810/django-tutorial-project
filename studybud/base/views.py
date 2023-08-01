04c580a03542e0e75ad76716a42b65245387b39b
Deepanshu0810 <deepanshu0810@gmail.com>
2023-08-01 23:44:03 +0530

    ADD: user-login-logout

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 from .models import Room, Topic
+from django.contrib.auth.models import User
 from .forms import RoomForm
 
 # Create your views here.
@@ -61,3 +64,30 @@ def deleteRoom(request,pk):
     
     context = {'obj':room}
     return render(request,'delete.html',context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+
+        except:
+            # we will use django flash messages
+            messages.error(request,'Username does not exist')
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Username OR password is incorrect')
+
+    context = {}
+    return render(request,'login_register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
