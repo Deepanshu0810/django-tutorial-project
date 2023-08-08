2efee98083cb37c4610ae66c011d1546e7f2190b
Deepanshu0810 <deepanshu0810@gmail.com>
2023-08-08 10:45:56 +0530

    hosting-setup

--- studybud/build_files.sh
+++ studybud/build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
 echo "BUILDING STUDYBUD"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo "BUILD COMPLETE"
